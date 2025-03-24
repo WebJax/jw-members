5c300b56140cd9e130d9c0d366b339afb547e640
Jacob Thygesen <info@jaxweb.dk>
2025-03-24 02:35:48 +0100

    Create class-jw-members.php

---
+++ includes/class-jw-members.php
@@ -0,0 +1,12 @@
+<?php
+
+class JW_Members {
+    public function run() {
+        // Add your initialization code here
+        add_action('init', array($this, 'init'));
+    }
+
+    public function init() {
+        // Add your custom initialization code here
+    }
+}
